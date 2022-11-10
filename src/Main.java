300e36739336f4746495ae2151cbdaa7e626ebb8
Айрат <airat_vip@mail.ru>
2022-11-10 21:45:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws WrongPasswordException, WrongLoginException {
+        Data.checkingDataEntry("123","123", "123"  );
+
+    }
+}
